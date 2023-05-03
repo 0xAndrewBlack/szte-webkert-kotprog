de9197b1abfe4d455ea92f38598c177f039afe92
Lemon <andras@fekete.io>
2023-05-03 15:16:35 +0200

    🔥 feat: Add Fire. (#4)
    * 🔥 feat: Firebase and Firestore added.
    
    * 📦 build: Firebase CI/CD actions.
    
    * 🔑 feat: Added environments.
    
    * 🐛 build: Testing path fix.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyB6WzFKkNhXHdNCKT2cBFBXLpkvf0pOsEg',
+    authDomain: 'webkert-ff294.firebaseapp.com',
+    projectId: 'webkert-ff294',
+    storageBucket: 'webkert-ff294.appspot.com',
+    messagingSenderId: '1046679094100',
+    appId: '1:1046679094100:web:475c3ad69279666841fec1',
+    measurementId: 'G-EDP5N03PMW',
+  },
+  production: false,
+};
